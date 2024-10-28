36a2daeb27afe4efd195c253e6ab25dc914e0a75
BenhurUFSM <benhur@inf.ufsm.br>
2024-10-28 19:34:27 -0300

    adiciona código mínimo em so.c para usar MMU

--- Trabalhos/t2/Codigo/so.c
+++ Trabalhos/t2/Codigo/so.c
@@ -8,7 +8,7 @@
 #include "dispositivos.h"
 #include "irq.h"
 #include "programa.h"
-#include "instrucao.h"
+#include "tabpag.h"
 
 #include <stdlib.h>
 #include <stdbool.h>
@@ -18,6 +18,27 @@
 // intervalo entre interrupções do relógio
 #define INTERVALO_INTERRUPCAO 50   // em instruções executadas
 
+// Não tem processos nem memória virtual, mas é preciso usar a paginação,
+//   pelo menos para implementar relocação, já que os programas estão sendo
+//   todos montados para serem executados no endereço 0 e o endereço 0
+//   físico é usado pelo hardware nas interrupções.
+// Os programas estão sendo carregados no início de um quadro, e usam quantos
+//   quadros forem necessárias. Para isso a variável quadro_livre contém
+//   o número do primeiro quadro da memória principal que ainda não foi usado.
+//   Na carga do processo, a tabela de páginas (deveria ter uma por processo,
+//   mas não tem processo) é alterada para que o endereço virtual 0 resulte
+//   no quadro onde o programa foi carregado. Com isso, o programa carregado
+//   é acessível, mas o acesso ao anterior é perdido.
+
+// t2: a interface de algumas funções que manipulam memória teve que ser alterada,
+//   para incluir o processo ao qual elas se referem. Para isso, precisa de um
+//   tipo para o processo. Neste código, não tem processos implementados, e não
+//   tem um tipo para isso. Chutei o tipo int. Foi necessário também um valor para
+//   representar a inexistência de um processo, coloquei -1. Altere para o seu
+//   tipo, ou substitua os usos de processo_t e NENHUM_PROCESSO para o seu tipo.
+typedef int processo_t;
+#define NENHUM_PROCESSO -1
+
 struct so_t {
   cpu_t *cpu;
   mem_t *mem;
@@ -26,6 +47,15 @@ struct so_t {
   console_t *console;
   bool erro_interno;
   // t1: tabela de processos, processo corrente, pendências, etc
+
+  // primeiro quadro da memória que está livre (quadros anteriores estão ocupados)
+  // t2: com memória virtual, o controle de memória livre e ocupada é mais
+  //     completo que isso
+  int quadro_livre;
+  // uma tabela de páginas para poder usar a MMU
+  // t2: com processos, não tem esta tabela global, tem que ter uma para
+  //     cada processo
+  tabpag_t *tabpag_global;
 };
 
 
@@ -33,10 +63,13 @@ struct so_t {
 static int so_trata_interrupcao(void *argC, int reg_A);
 
 // funções auxiliares
-// carrega o programa contido no arquivo na memória do processador; retorna end. inicial
-static int so_carrega_programa(so_t *self, char *nome_do_executavel);
-// copia para str da memória do processador, até copiar um 0 (retorna true) ou tam bytes
-static bool copia_str_da_mem(int tam, char str[tam], mem_t *mem, int ender);
+// no t2, foi adicionado o 'processo' aos argumentos dessas funções 
+// carrega o programa na memória virtual de um processo; retorna end. inicial
+static int so_carrega_programa(so_t *self, processo_t processo,
+                               char *nome_do_executavel);
+// copia para str da memória do processo, até copiar um 0 (retorna true) ou tam bytes
+static bool so_copia_str_do_processo(so_t *self, int tam, char str[tam],
+                                     int end_virt, processo_t processo);
 
 // CRIAÇÃO {{{1
 
@@ -66,7 +99,7 @@ so_t *so_cria(cpu_t *cpu, mem_t *mem, mmu_t *mmu,
   //   de interrupção (escrito em asm). esse programa deve conter a 
   //   instrução CHAMAC, que vai chamar so_trata_interrupcao (como
   //   foi definido acima)
-  int ender = so_carrega_programa(self, "trata_int.maq");
+  int ender = so_carrega_programa(self, NENHUM_PROCESSO, "trata_int.maq");
   if (ender != IRQ_END_TRATADOR) {
     console_printf("SO: problema na carga do programa de tratamento de interrupção");
     self->erro_interno = true;
@@ -78,6 +111,16 @@ so_t *so_cria(cpu_t *cpu, mem_t *mem, mmu_t *mmu,
     self->erro_interno = true;
   }
 
+  // inicializa a tabela de páginas global, e entrega ela para a MMU
+  // t2: com processos, essa tabela não existiria, teria uma por processo, que
+  //     deve ser colocada na MMU quando o processo é despachado para execução
+  self->tabpag_global = tabpag_cria();
+  mmu_define_tabpag(self->mmu, self->tabpag_global);
+  // define o primeiro quadro livre de memória como o seguinte àquele que
+  //   contém o endereço 99 (as 100 primeiras posições de memória (pelo menos)
+  //   não vão ser usadas por programas de usuário)
+  // t2: o controle de memória livre deve ser mais aprimorado que isso  
+  self->quadro_livre = 99 / TAM_PAGINA + 1;
   return self;
 }
 
@@ -203,15 +246,17 @@ static void so_trata_irq_reset(so_t *self)
   //   registradores diretamente para a memória, de onde a CPU vai carregar
   //   para os seus registradores quando executar a instrução RETI
 
-  // coloca o programa init na memória
-  int ender = so_carrega_programa(self, "init.maq");
-  if (ender != 100) {
+  // coloca o programa "init" na memória
+  // t2: deveria criar um processo, e programar a tabela de páginas dele
+  processo_t processo = 1; // deveria inicializar um processo...
+  int ender = so_carrega_programa(self, processo, "init.maq");
+  if (ender != 0) {
     console_printf("SO: problema na carga do programa inicial");
     self->erro_interno = true;
     return;
   }
 
-  // altera o PC para o endereço de carga
+  // altera o PC para o endereço de carga (deve ter sido o endereço virtual 0)
   mem_escreve(self->mem, IRQ_END_PC, ender);
   // passa o processador para modo usuário
   mem_escreve(self->mem, IRQ_END_modo, usuario);
@@ -385,16 +430,18 @@ static void so_chamada_cria_proc(so_t *self)
   // ainda sem suporte a processos, carrega programa e passa a executar ele
   // quem chamou o sistema não vai mais ser executado, coitado!
   // T1: deveria criar um novo processo
+  processo_t processo = 1; // T2: o processo criado
 
   // em X está o endereço onde está o nome do arquivo
   int ender_proc;
   // t1: deveria ler o X do descritor do processo criador
   if (mem_le(self->mem, IRQ_END_X, &ender_proc) == ERR_OK) {
     char nome[100];
-    if (copia_str_da_mem(100, nome, self->mem, ender_proc)) {
-      int ender_carga = so_carrega_programa(self, nome);
-      if (ender_carga > 0) {
-        // t1: deveria escrever no PC do descritor do processo criado
+    if (so_copia_str_do_processo(self, 100, nome, ender_proc, processo)) {
+      int ender_carga = so_carrega_programa(self, processo, nome);
+      // o endereço de carga é endereço virtual, deve ser 0
+      if (ender_carga == 0) {
+        // deveria escrever no PC do descritor do processo criado
         mem_escreve(self->mem, IRQ_END_PC, ender_carga);
         return;
       } // else?
@@ -427,43 +474,110 @@ static void so_chamada_espera_proc(so_t *self)
 
 // CARGA DE PROGRAMA {{{1
 
-// carrega o programa na memória
+// funções auxiliares
+static int so_carrega_programa_na_memoria_fisica(so_t *self, programa_t *programa);
+static int so_carrega_programa_na_memoria_virtual(so_t *self,
+                                                  programa_t *programa,
+                                                  processo_t processo);
+
+// carrega o programa na memória de um processo ou na memória física se NENHUM_PROCESSO
 // retorna o endereço de carga ou -1
-static int so_carrega_programa(so_t *self, char *nome_do_executavel)
+static int so_carrega_programa(so_t *self, processo_t processo,
+                               char *nome_do_executavel)
 {
-  // programa para executar na nossa CPU
-  programa_t *prog = prog_cria(nome_do_executavel);
-  if (prog == NULL) {
+  console_printf("SO: carga de '%s'", nome_do_executavel);
+
+  programa_t *programa = prog_cria(nome_do_executavel);
+  if (programa == NULL) {
     console_printf("Erro na leitura do programa '%s'\n", nome_do_executavel);
     return -1;
   }
 
-  int end_ini = prog_end_carga(prog);
-  int end_fim = end_ini + prog_tamanho(prog);
+  int end_carga;
+  if (processo == NENHUM_PROCESSO) {
+    end_carga = so_carrega_programa_na_memoria_fisica(self, programa);
+  } else {
+    end_carga = so_carrega_programa_na_memoria_virtual(self, programa, processo);
+  }
+
+  prog_destroi(programa);
+  return end_carga;
+}
+
+static int so_carrega_programa_na_memoria_fisica(so_t *self, programa_t *programa)
+{
+  int end_ini = prog_end_carga(programa);
+  int end_fim = end_ini + prog_tamanho(programa);
 
   for (int end = end_ini; end < end_fim; end++) {
-    if (mem_escreve(self->mem, end, prog_dado(prog, end)) != ERR_OK) {
+    if (mem_escreve(self->mem, end, prog_dado(programa, end)) != ERR_OK) {
       console_printf("Erro na carga da memória, endereco %d\n", end);
       return -1;
     }
   }
-
-  prog_destroi(prog);
-  console_printf("SO: carga de '%s' em %d-%d", nome_do_executavel, end_ini, end_fim);
+  console_printf("carregado na memória física, %d-%d", end_ini, end_fim);
   return end_ini;
+}
+
+static int so_carrega_programa_na_memoria_virtual(so_t *self,
+                                                  programa_t *programa,
+                                                  processo_t processo)
+{
+  // t2: isto tá furado...
+  // está simplesmente lendo para o próximo quadro que nunca foi ocupado,
+  //   nem testa se tem memória disponível
+  // com memória virtual, a forma mais simples de implementar a carga de um
+  //   programa é carregá-lo para a memória secundária, e mapear todas as páginas
+  //   da tabela de páginas do processo como inválidas. Assim, as páginas serão
+  //   colocadas na memória principal por demanda. Para simplificar ainda mais, a
+  //   memória secundária pode ser alocada da forma como a principal está sendo
+  //   alocada aqui (sem reuso)
+  int end_virt_ini = prog_end_carga(programa);
+  int end_virt_fim = end_virt_ini + prog_tamanho(programa) - 1;
+  int pagina_ini = end_virt_ini / TAM_PAGINA;
+  int pagina_fim = end_virt_fim / TAM_PAGINA;
+  int quadro_ini = self->quadro_livre;
+  // mapeia as páginas nos quadros
+  int quadro = quadro_ini;
+  for (int pagina = pagina_ini; pagina <= pagina_fim; pagina++) {
+    tabpag_define_quadro(self->tabpag_global, pagina, quadro);
+    quadro++;
+  }
+  self->quadro_livre = quadro;
+
+  // carrega o programa na memória principal
+  int end_fis_ini = quadro_ini * TAM_PAGINA;
+  int end_fis = end_fis_ini;
+  for (int end_virt = end_virt_ini; end_virt <= end_virt_fim; end_virt++) {
+    if (mem_escreve(self->mem, end_fis, prog_dado(programa, end_virt)) != ERR_OK) {
+      console_printf("Erro na carga da memória, end virt %d fís %d\n", end_virt,
+                     end_fis);
+      return -1;
+    }
+    end_fis++;
+  }
+  console_printf("carregado na memória virtual V%d-%d F%d-%d",
+                 end_virt_ini, end_virt_fim, end_fis_ini, end_fis - 1);
+  return end_virt_ini;
 }
 
 // ACESSO À MEMÓRIA DOS PROCESSOS {{{1
 
-// copia uma string da memória do simulador para o vetor str.
+// copia uma string da memória do processo para o vetor str.
 // retorna false se erro (string maior que vetor, valor não char na memória,
 //   erro de acesso à memória)
-// T1: deveria verificar se a memória pertence ao processo
-static bool copia_str_da_mem(int tam, char str[tam], mem_t *mem, int ender)
+// O endereço é um endereço virtual de um processo.
+// T2: Com memória virtual, cada valor do espaço de endereçamento do processo
+//   pode estar em memória principal ou secundária (e tem que achar onde)
+static bool so_copia_str_do_processo(so_t *self, int tam, char str[tam],
+                                     int end_virt, processo_t processo)
 {
+  if (processo == NENHUM_PROCESSO) return false;
   for (int indice_str = 0; indice_str < tam; indice_str++) {
     int caractere;
-    if (mem_le(mem, ender + indice_str, &caractere) != ERR_OK) {
+    // não tem memória virtual implementada, posso usar a mmu para traduzir
+    //   os endereços e acessar a memória
+    if (mmu_le(self->mmu, end_virt + indice_str, &caractere, usuario) != ERR_OK) {
       return false;
     }
     if (caractere < 0 || caractere > 255) {
